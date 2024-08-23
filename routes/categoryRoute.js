53e872b6cd296269e369d29995a176533ed7858d
Ballslamic <71572589+Ballslamic@users.noreply.github.com>
2024-08-23 13:46:04 -0400

    Comment + format: routes\

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../models/categoryModel");
 
-// Route to display all categories (you can render this on the categories page)
+// Route to display all categories
 router.get("/categories", async (req, res) => {
   try {
-    const categories = await Category.find({});
-    res.render("categories", { categories });
+    const categories = await Category.find({});  // Fetch all categories from the database
+    res.render("categories", { categories });  // Render the categories view with the fetched categories
   } catch (error) {
     console.error("Error fetching categories:", error);
     res.status(500).send("Internal Server Error");
@@ -16,15 +16,15 @@ router.get("/categories", async (req, res) => {
 // Route to create a new category
 router.post("/categories/add", async (req, res) => {
   try {
-    const { categoryName, colorCode } = req.body;
+    const { categoryName, colorCode } = req.body;  // Extract data from the request body
 
     const newCategory = new Category({
       name: categoryName,
       colorCode: colorCode,
     });
 
-    await newCategory.save();
-    res.redirect("/categories");
+    await newCategory.save();  // Save the new category to the database
+    res.redirect("/categories");  // Redirect to the categories page after creation
   } catch (error) {
     console.error("Error creating category:", error);
     res.status(500).send("Internal Server Error");
@@ -39,7 +39,7 @@ router.post("/categories/edit/:id", async (req, res) => {
       name: categoryName,
       colorCode: colorCode,
     });
-    res.redirect("/categories");
+    res.redirect("/categories");  // Redirect to the categories page after editing
   } catch (error) {
     console.error("Error editing category:", error);
     res.status(500).send("Internal Server Error");
@@ -49,8 +49,8 @@ router.post("/categories/edit/:id", async (req, res) => {
 // Route to delete a category
 router.post("/categories/delete/:id", async (req, res) => {
   try {
-    await Category.findByIdAndDelete(req.params.id);
-    res.redirect("/categories");
+    await Category.findByIdAndDelete(req.params.id);  // Delete the category from the database
+    res.redirect("/categories");  // Redirect to the categories page after deletion
   } catch (error) {
     console.error("Error deleting category:", error);
     res.status(500).send("Internal Server Error");
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,20 +4,22 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// Tests user route to make sure it is working
 router.get("/", (req, res) => {
   res.send("User route is working");
 });
 
-// Register user
+// Register a new user
 router.post("/register", async (req, res) => {
   try {
-    const { userName, email, password } = req.body;
-    var user = await User.findOne({ email });
+    const { userName, email, password } = req.body; // Extract data from the request body
+
+    var user = await User.findOne({ email }); // Searches if the EXACT (case-sensitive) email adress is in use
     if (user) {
       throw new Error("Email already in use");
     }
 
-    user = await User.findOne({
+    user = await User.findOne({ // Searches if the userName (case in-sensitive for uniqueness) is in use
       userName: {
         $regex: new RegExp(userName, "i"),
       }
@@ -26,19 +28,19 @@ router.post("/register", async (req, res) => {
       throw new Error("User name already in use");
     }
 
-    user = new User({ userName, email, password });
-    await user.save();
+    user = new User({ userName, email, password }); // Registers new user with the given information
+    await user.save(); // Makes sure it saves properly
     res.status(201).send({ user, message: "User created successfully" });
   } catch (err) {
-    res.status(400).send({ error: "Unable to register" + err });
+    res.status(400).send({ error: "Unable to register: " + err });
   }
 });
 
 // login as a user
 router.post("/login", async (req, res) => {
   try {
-    const { userName , password } = req.body;
-    const user = await User.findOne({
+    const { userName, password } = req.body; // Extract data from the request body
+    const user = await User.findOne({ // Searches if the userName (case in-sensitive for uniqueness) is in the database
       userName: {
         $regex: new RegExp(userName, "i"),
       }
@@ -48,19 +50,19 @@ router.post("/login", async (req, res) => {
       throw new Error("Unable to login / User not found. D:");
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password); // Checks if the password matches the hased password in the database
     if (!isMatch) {
-      throw new Error("Unable to login / wrong password. D: ");
+      throw new Error("Unable to login / wrong password. D: "); // Throws this error if the password is incorrect
     }
 
-    const token = jwt.sign(
+    const token = jwt.sign( // Creates an unlimited-time token (for our testing purposes) linked to the user's login for use in authentication
       { _id: user._id.toString() },
       process.env.JWT_SECRET_KEY
     );
 
-    res.status(201).send({ user, token, message: "Logged in successfully!! :D" });
+    res.status(201).send({ user, token, message: "Logged in successfully!! :D" }); // Returns the user, their token, and a short message if successful
   } catch (err) {
-    res.status(400).send({ error: "Unable to Login???? " + err });
+    res.status(400).send({ error: "Unable to Login???? " + err }); // Returns error + more information
   }
 });
 
